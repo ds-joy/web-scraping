b13ec4381f4a6b4b89699733cd3d705209cbda5b
Debasish Sarker <debasishsarker36@gmail.com>
2021-07-19 20:50:35 +0600

    test

--- README.md
+++ README.md
@@ -10,5 +10,20 @@
 scrapy genspider countries www.worldometers.info/world-population/population-by-country
 
 
+### <i>Section 04 lesson 01</i>
+```python
+def parse(self, response):
 
+    # gets all the countries 
+    countries = response.xpath("//td/a")
 
+    #for each country get the country name and country link
+    for country in countries:
+        name = country.xpath(".//text()").get()
+        link = country.xpath(".//@href").get()
+
+        yield {
+            'country_name': name,
+            'country_link': link
+        }
+```
--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -7,13 +7,20 @@ class CountriesSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-       countries = response.xpath("//td/a")
+        countries = response.xpath("//td/a")
 
-       for country in countries:
-           name = country.xpath(".//text()").get()
-           link = country.xpath(".//@href").get()
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
 
-           yield {
-                'country_name': name,
-                'country_link': link
-            }
+            # yield {
+            #         'country_name': name,
+            #         'country_link': link
+            #     }
+
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+            # yield scrapy.Request(url=absolute_url)
+
+            yield response.follow(url=link)
+
