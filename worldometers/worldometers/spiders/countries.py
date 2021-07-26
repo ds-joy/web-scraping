e251122fbaeaf5675cee478f66e3aaaf96c422bc
Debasish Sarker <debasishsarker36@gmail.com>
2021-07-26 10:20:40 +0600

    scraping all the country links

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -5,21 +5,24 @@ class CountriesSpider(scrapy.Spider):
 
     name = 'countries'
 
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
 
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        # title = response.xpath("//h1/text()").get()
         countries = response.xpath("//td/a")
-
         for country in countries:
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
-            yield {
-                'name': name,
-                'link': link,
-            }
 
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+            # yield scrapy.Request(url=absolute_url)
 
+            yield response.follow(url=link)
+
+            # yield {
+            #     'name': name,
+            #     'link': link,
+            # }
